67a3202eba79255fca198e21daf4b62d52f3a245
apptest <appTest1@wavemaker.com>
2017-10-31 20:41:58 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,14 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "default",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_jAPoZSCXTo",
+  "homePage" : "Main",
+  "name" : "Auto_jAPoZSCXTo",
+  "platformType" : "DEFAULT",
+  "securityEnabled" : "false",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "PREFAB",
+  "version" : "1.0"
+};
